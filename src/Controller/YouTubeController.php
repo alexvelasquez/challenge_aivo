7543e97251b0e0f1c1ff5d2534ca9e300cfd3ae4
alexvelasquez <alexvelasquez94@gmail.com>
2021-06-27 14:14:23 -0300

    nelmio api doc configuration

--- src/Controller/YouTubeController.php
+++ src/Controller/YouTubeController.php
@@ -8,6 +8,9 @@ use FOS\RestBundle\Request\ParamFetcherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 use App\Service\ApiYouTubeService;
 
@@ -19,11 +22,18 @@ class YouTubeController extends AbstractFOSRestController
 	  /**
      * @Rest\Get("")
      * Returns up to 10 results from a search on youtube given a keyword.
-     * @Rest\QueryParam(name="search", description="search keyword")
-     * @Rest\QueryParam(name="max_result", description="max result")
-     * 
+     * @Rest\QueryParam(name="search")
+     * @Rest\QueryParam(name="max_result")
+     * @OA\Response(response=200,description="Returns the videos of a specific search")
+     * @OA\Response(response=400,description="Parameter search: This value should not be null.")
+     * @OA\Response(response=404,description="Video not found.")
+     * @OA\Parameter(name="search",in="query",description="search keyword",@OA\Schema(type="string"))
+     * @OA\Parameter(name="max_result",in="query",description="maximum number of results by default returns 10",@OA\Schema(type="string"))
+     * @OA\Tag(name="YouTube")
+     * @Security(name="Bearer")
      * @param ParamFetcherInterface $paramFetcher
      * @return Response
+     *
     */
     public function search(ParamFetcherInterface $paramFetcher, ApiYouTubeService $apiService): Response
     {
