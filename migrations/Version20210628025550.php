b0b32912da81c2cd7dbab7b39036493d2526dc4c
alexvelasquez <alexvelasquez94@gmail.com>
2021-06-28 00:15:02 -0300

    fix migration

--- migrations/Version20210628025550.php
+++ migrations/Version20210628025550.php
@@ -6,27 +6,27 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210627152853 extends AbstractMigration
+final class Version20210628025550 extends AbstractMigration
 {
-
-    public function getDescription() : string
+    public function getDescription(): string
     {
-        return '';
+        return 'create authenticator user';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $now = (new \DateTime())->format('Y-m-d H:m:s');
         $this->addSql( "INSERT INTO user (username, password, created_at, updated_at) VALUES ('ws_aivo', 'tkWgUrVTNzMhXyJeeAiebfI2+xKmgLXsLHnE0SRZIs7mpubaQXMRo0P49dVzdv8ZHRG6cl8VNMQ8h1P1U5zAhQ==', '{$now}', '{$now}')");
 
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
     }
 }
